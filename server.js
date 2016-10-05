76d4f19009d362ba5092e296e655ff97c77ee822
Rasmus Jakobsson <Rasmus Jakobsson>
2016-10-05 22:26:32 +0200

    test3

--- server.js
+++ server.js
@@ -8,16 +8,15 @@ app.use(express.static(__dirname+"/public"));
 
 app.get('/', function(req,res){
     var currentTime = new Date();
-    console.log("User connected! "+currentTime)
+    console.log(req.ip+" connected! "+currentTime)
     res.sendFile(__dirname+"/index.html")
 });
 
 var server = http.listen(port,"192.168.0.11", function () {
-    
+
   var host = server.address().address;
   var port = server.address().port;
 
   console.log("Example app listening at http://%s:%s", host, port)
 
 });
-	
